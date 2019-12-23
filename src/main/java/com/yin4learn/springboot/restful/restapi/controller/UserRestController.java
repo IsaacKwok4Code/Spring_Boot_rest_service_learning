88806fc12d6963bb5546e040f8a7f61f579e37ed
unknown <isaac4learn@gmail.com>
2019-12-23 11:29:22 -0600

    added simple function

--- src/main/java/com/yin4learn/springboot/restful/restapi/controller/UserRestController.java
+++ src/main/java/com/yin4learn/springboot/restful/restapi/controller/UserRestController.java
@@ -1,15 +1,26 @@
 package com.yin4learn.springboot.restful.restapi.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yin4learn.springboot.restful.restapi.dto.UserDTO;
+import com.yin4learn.springboot.restful.restapi.model.UserDetailsRequest;
+import com.yin4learn.springboot.restful.restapi.model.UserDetailsResponse;
+import com.yin4learn.springboot.restful.restapi.service.UserService;
 
 @RestController
 @RequestMapping("user")// http://localhost:8080/user
 public class UserRestController {
+	
+	@Autowired
+	UserService userService;
 
 	@GetMapping("/getUser")
 	public String getUser() {
@@ -17,8 +28,13 @@ public class UserRestController {
 	}
 	
 	@PostMapping("/createUser")
-	public String createUser() {
-		return "post function";
+	public UserDetailsResponse createUser(@RequestBody UserDetailsRequest userinfo) {
+		UserDetailsResponse userinfoRs = new UserDetailsResponse();
+		UserDTO userDTO = new UserDTO();
+		BeanUtils.copyProperties(userinfo, userDTO);
+		UserDTO createdObj = userService.createUser(userDTO);
+		BeanUtils.copyProperties(createdObj, userinfoRs);
+		return userinfoRs;
 	}
 	
 	@PutMapping("/updateUser")
---
+++ src/main/java/com/yin4learn/springboot/restful/restapi/model/UserDetailsResponse.java
@@ -0,0 +1,45 @@
+package com.yin4learn.springboot.restful.restapi.model;
+
+public class UserDetailsResponse {
+
+	private String userId;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
