6bc6eeb72c5f5c483a91b1571ee476a483161d43
unknown <isaac4learn@gmail.com>
2019-12-23 17:21:37 -0600

    added the dto and entity service

---
+++ src/main/java/com/yin4learn/springboot/restful/restapi/service/UserService.java
@@ -0,0 +1,9 @@
+package com.yin4learn.springboot.restful.restapi.service;
+
+import com.yin4learn.springboot.restful.restapi.dto.UserDTO;
+
+public interface UserService {
+
+	UserDTO createUser(UserDTO user);
+	
+}
---
+++ src/main/java/com/yin4learn/springboot/restful/restapi/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.yin4learn.springboot.restful.restapi.service;
+
+import org.springframework.stereotype.Service;
+
+import com.yin4learn.springboot.restful.restapi.dto.UserDTO;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDTO createUser(UserDTO user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
